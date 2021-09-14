604b0c1b3efe05452119bc4dbe76d3ef3500810f
VCKazakova <v.c.kazakova@gmail.com>
2021-09-14 09:35:57 +0300

    Initial commit

---
+++ src/main/java/com/kazakova/clothesweather/ClothesWeatherApplication.java
@@ -0,0 +1,13 @@
+package com.kazakova.clothesweather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothesWeatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClothesWeatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kazakova/clothesweather/ClothesWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kazakova.clothesweather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothesWeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
