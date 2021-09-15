942f8cf3566b6cbe0f4b57383b2a73201dcb9b07
VCKazakova <v.c.kazakova@gmail.com>
2021-09-15 10:36:34 +0300

    master create controller

---
+++ src/main/java/com/kazakova/clothesweather/controller/WardrobeController.java
@@ -0,0 +1,32 @@
+package com.kazakova.clothesweather.controller;
+
+
+import com.kazakova.clothesweather.model.Season;
+import com.kazakova.clothesweather.model.Wardrobe;
+import com.kazakova.clothesweather.service.WardrobeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class WardrobeController {
+
+    private final WardrobeService wardrobeService;
+
+
+    public WardrobeController(WardrobeService wardrobeService) {
+        this.wardrobeService = wardrobeService;
+    }
+
+    @GetMapping("/season/{season}")
+    public List<Wardrobe> getAllBySeason(@PathVariable("season") Season season) {
+        log.info(">> WardrobeController getAllBySeason  season={}", season);
+        List<Wardrobe> wardrobe = wardrobeService.findAllBySeason(season);
+        log.info(">> WardrobeController getAllBySeason wardrobe={}", wardrobe);
+        return wardrobe;
+    }
+}
