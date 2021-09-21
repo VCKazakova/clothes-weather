89bcae5f4b86652a352de220c31748faed30ef8a
VCKazakova <v.c.kazakova@gmail.com>
2021-09-21 08:25:56 +0300

    master update controller and create hello-page.html

--- src/main/java/com/kazakova/clothesweather/controller/LoginController.java
+++ src/main/java/com/kazakova/clothesweather/controller/LoginController.java
@@ -1,22 +1,25 @@
 package com.kazakova.clothesweather.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
+    @GetMapping("/")
     public String helloPage() {
-        return "/";
+        return "hello-page";
     }
-
+    @GetMapping("/home")
     public String homePage() {
         return "home";
     }
-
+    @GetMapping("/login")
     public String login() {
         return "login";
     }
 
+    @GetMapping("/create-account")
     public String createAccount() {
         return "create-account";
     }
