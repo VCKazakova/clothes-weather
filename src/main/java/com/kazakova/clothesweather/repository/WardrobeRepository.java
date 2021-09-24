dd6f6f19be5e880c351d81f3b9554bc93c3117f4
VCKazakova <v.c.kazakova@gmail.com>
2021-09-24 10:26:13 +0300

    master update WardrobeRepository ana WardrobeService

--- src/main/java/com/kazakova/clothesweather/repository/WardrobeRepository.java
+++ src/main/java/com/kazakova/clothesweather/repository/WardrobeRepository.java
@@ -2,8 +2,6 @@ package com.kazakova.clothesweather.repository;
 
 
 import com.kazakova.clothesweather.model.Season;
-import com.kazakova.clothesweather.model.Style;
-import com.kazakova.clothesweather.model.Type;
 import com.kazakova.clothesweather.model.Wardrobe;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,11 +12,5 @@ import java.util.List;
 public interface WardrobeRepository extends JpaRepository<Wardrobe, Long> {
 
     List<Wardrobe> findAllBySeason(Season season);
-    List<Wardrobe> findAllByStyle(Style style);
-    List<Wardrobe> findAllByType(Type type);
-    List<Wardrobe> findAllPhotoBySeason(Season season);
-    List<Wardrobe> findAllPhotoByStyle(Style style);
-    List<Wardrobe> findAllPhotoByType(Type type);
-    List<Wardrobe> findAllPhotoBySeasonAndStyleAndType(Season season, Style style, Type type);
 
 }
--- src/main/java/com/kazakova/clothesweather/service/WardrobeService.java
+++ src/main/java/com/kazakova/clothesweather/service/WardrobeService.java
@@ -2,16 +2,16 @@ package com.kazakova.clothesweather.service;
 
 
 import com.kazakova.clothesweather.model.Season;
-import com.kazakova.clothesweather.model.Style;
-import com.kazakova.clothesweather.model.Type;
 import com.kazakova.clothesweather.model.Wardrobe;
 import com.kazakova.clothesweather.repository.WardrobeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.SQLException;
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,48 +20,25 @@ public class WardrobeService {
     @Autowired
     private final WardrobeRepository wardrobeRepository;
 
+    @Transactional(rollbackFor = {SQLException.class})
     public Wardrobe createStuff(Wardrobe wardrobe) {
         return wardrobeRepository.save(wardrobe);
     }
 
-    public Optional<Wardrobe> findStuffById(Long id) {
-        return wardrobeRepository.findById(id);
-    }
-
-    public List<Wardrobe> findAll() {
-        return wardrobeRepository.findAll();
-    }
-
+    @Transactional(rollbackFor = {SQLException.class})
     public void deleteStuffById(Long id) {
         wardrobeRepository.deleteById(id);
     }
 
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
+    public List<Wardrobe> findAll() {
+        return wardrobeRepository.findAll();
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS, readOnly = true)
     public List<Wardrobe> findAllBySeason(Season season) {
         return wardrobeRepository.findAllBySeason(season);
     }
 
-    public List<Wardrobe> findAllByStyle(Style style) {
-        return wardrobeRepository.findAllByStyle(style);
-    }
-
-    public List<Wardrobe> findAllByType(Type type) {
-        return wardrobeRepository.findAllByType(type);
-    }
-
-    public List<Wardrobe> findAllPhotoBySeason(Season season) {
-        return wardrobeRepository.findAllPhotoBySeason(season);
-    }
-
-    public List<Wardrobe> findAllPhotoByStyle(Style style) {
-        return wardrobeRepository.findAllPhotoByStyle(style);
-    }
-
-    public List<Wardrobe> findAllPhotoByType(Type type) {
-        return wardrobeRepository.findAllPhotoByType(type);
-    }
-
-    public List<Wardrobe> findAllPhotoBySeasonAndStyleAndType(Season season, Style style, Type type) {
-        return wardrobeRepository.findAllPhotoBySeasonAndStyleAndType(season, style, type);
-    }
 
 }
