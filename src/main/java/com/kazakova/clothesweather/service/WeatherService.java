c8c6a5d1d88e81568fdf8b2906459ef0065a01e8
VCKazakova <v.c.kazakova@gmail.com>
2021-09-25 12:55:40 +0300

    master add WeatherService

---
+++ src/main/java/com/kazakova/clothesweather/service/WeatherService.java
@@ -0,0 +1,93 @@
+package com.kazakova.clothesweather.service;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+@Service
+public class WeatherService {
+
+    public static Document getPage() throws IOException {
+        try {
+            String url = "https://pogoda.mail.ru/prognoz/moskva/";
+            Document page = Jsoup.parse(new URL(url), 100000);
+            return page;
+        } catch (Exception exception) {
+            throw new IOException("The page doesn't found");
+        }
+    }
+
+
+    public static Elements selectInformationAboutTodayWeather(Document page) {
+        return Objects.requireNonNull(page.select("div[class=information__content]")
+                        .first())
+                .select("div[class=information__content__temperature]");
+    }
+
+    public String getTodayWeather() throws IOException {
+        try {
+            Elements values = selectInformationAboutTodayWeather(getPage());
+            String todayTemperature = values.text();
+
+            Elements span = values.select("span");
+            List<String> spanList = span.stream().map(Node::toString).collect(Collectors.toList());
+            String stringFromList = spanList.toString();
+
+
+            String pattern = "([а-яА-Я]+)(.*)([а-яА-Я]+)";
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(stringFromList);
+
+            if (m.find()) {
+                return "Погода на сегодня: " + todayTemperature + " " + m.group(0);
+            } else throw new IOException("The weather doesn't find");
+        } catch (Exception exception) {
+            throw new IOException("The weather doesn't found");
+        }
+    }
+
+    public static Element selectInformationAboutTomorrowWeather(Document page) {
+        return Objects.requireNonNull(page.select("div[class=days]")
+                        .first())
+                .select("div[class=weather-icon weather-icon_12 margin-bottom-10]").first();
+    }
+
+    public static String selectInformationAboutTomorrowTemperature(Document page) {
+        return Objects.requireNonNull(Objects.requireNonNull(page.select("div[class=days]")
+                        .first())
+                .select("div[class=day__temperature]").first()).text();
+    }
+
+
+    public String getTomorrowWeather() throws IOException {
+        try {
+            Element value = selectInformationAboutTomorrowWeather(getPage());
+            String tomorrowTemperature = selectInformationAboutTomorrowTemperature(getPage());
+
+            String valueToString = value.toString();
+
+            String pattern = "([а-яА-Я]+)(.*)([а-яА-Я]+)";
+            Pattern r = Pattern.compile(pattern);
+            Matcher m = r.matcher(valueToString);
+
+            if (m.find()) {
+                return "Погода на завтра: " + tomorrowTemperature + " " + m.group(0);
+            } else throw new IOException("The weather doesn't find");
+        } catch (Exception exception) {
+            throw new IOException("The weather doesn't found");
+        }
+    }
+
+
+}
