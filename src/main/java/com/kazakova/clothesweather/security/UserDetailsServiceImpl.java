ac4d1148ca048d40975dc14ce64e30d026875b17
VCKazakova <v.c.kazakova@gmail.com>
2021-09-27 10:06:17 +0300

    master create UserDetailsService

---
+++ src/main/java/com/kazakova/clothesweather/security/UserDetailsServiceImpl.java
@@ -0,0 +1,21 @@
+package com.kazakova.clothesweather.security;
+
+import com.kazakova.clothesweather.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        return new
+                UserDetailsImpl(userRepository.findOneByLogin(login)
+                .orElseThrow(IllegalArgumentException::new));
+    }
+
+}
