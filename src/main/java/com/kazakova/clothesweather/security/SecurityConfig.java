cf6fd056b4ddc88ac18021c85b20475dbde2b32a
VCKazakova <v.c.kazakova@gmail.com>
2021-09-27 10:01:53 +0300

    master create SecurityConfig

---
+++ src/main/java/com/kazakova/clothesweather/security/SecurityConfig.java
@@ -0,0 +1,65 @@
+package com.kazakova.clothesweather.security;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Qualifier("dataSource")
+    @Autowired
+    private DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/signUp/**").permitAll()
+                .antMatchers("/wardrobe").authenticated()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .usernameParameter("login")
+                .defaultSuccessUrl("/home")
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .rememberMe()
+                .rememberMeParameter("remember-me")
+                .tokenRepository(tokenRepository());
+        http.csrf().disable();
+    }
+
+    @Bean
+    public PersistentTokenRepository tokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
+        return tokenRepository;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder);
+    }
+
+}
