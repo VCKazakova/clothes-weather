0460ee732d31fa34c502f06e3cdbd4f8d94e8ec5
VCKazakova <v.c.kazakova@gmail.com>
2021-09-28 09:45:01 +0300

    master add logs to LoginController

--- src/main/java/com/kazakova/clothesweather/controller/LoginController.java
+++ src/main/java/com/kazakova/clothesweather/controller/LoginController.java
@@ -2,43 +2,46 @@ package com.kazakova.clothesweather.controller;
 
 import com.kazakova.clothesweather.form.UserForm;
 import com.kazakova.clothesweather.service.SignUpService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.servlet.http.HttpServletRequest;
-
 @Controller
+@Slf4j
 public class LoginController {
 
     @Autowired
     private SignUpService service;
 
     @GetMapping("/")
-    public String helloPage() {
+    public String getHelloPage() {
+        log.info(">> LoginController getHelloPage");
         return "hello-page";
     }
 
     @GetMapping("/home")
-    public String homePage() {
+    public String getHomePage() {
+        log.info(">> LoginController getHomePage");
         return "home";
     }
 
     @GetMapping("/login")
     public String login() {
+        log.info(">> LoginController login");
         return "login";
     }
 
     @GetMapping("/signUp")
     public String createAccount() {
+        log.info(">> LoginController createAccount");
         return "signUp";
     }
 
     @PostMapping("/signUp")
     public String signUp(UserForm userForm) {
+        log.info(">> LoginController signUp");
         service.signUp(userForm);
         return "redirect:/login";
     }
