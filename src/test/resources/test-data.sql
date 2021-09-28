f4982bcc1cfa44f4d68f23bd71db2cd067d50a77
VCKazakova <v.c.kazakova@gmail.com>
2021-09-28 11:09:27 +0300

    master create resources for tests

---
+++ src/test/resources/test-data.sql
@@ -0,0 +1,4 @@
+INSERT INTO clothes VALUES
+    (DEFAULT, 'Coat', 'COAT', 'CASUAL' , 'DEMI', '/home/vckazakova/JavaPractice/clothes-weather/src/main/resources/photos/1.jpeg'),
+    (DEFAULT, 'Boots', 'SHOES', 'CHIC', 'DEMI', '/home/vckazakova/JavaPractice/clothes-weather/src/main/resources/photos/2.jpg'),
+    (DEFAULT, 'Dress', 'DRESS', 'CASUAL', 'DEMI', 'https://ae01.alicdn.com/kf/HTB1egF7bznuK1RkSmFPq6AuzFXa3/-.jpg');
