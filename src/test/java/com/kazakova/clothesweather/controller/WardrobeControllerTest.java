9c474d9642b6c257954bba4c61dd10ddcd79cdfb
VCKazakova <v.c.kazakova@gmail.com>
2021-10-01 10:34:15 +0300

    master update WardrobeControllerTest

--- src/test/java/com/kazakova/clothesweather/controller/WardrobeControllerTest.java
+++ src/test/java/com/kazakova/clothesweather/controller/WardrobeControllerTest.java
@@ -4,20 +4,16 @@ import com.kazakova.clothesweather.model.Season;
 import com.kazakova.clothesweather.model.Style;
 import com.kazakova.clothesweather.model.Type;
 import com.kazakova.clothesweather.model.Wardrobe;
-import com.kazakova.clothesweather.repository.WardrobeRepository;
 import com.kazakova.clothesweather.service.WardrobeService;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -37,7 +33,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @DisplayName("WardrobeController должен")
 @AutoConfigureMockMvc
-//@WebMvcTest(WardrobeController.class)
 @WithUserDetails("vika")
 @TestPropertySource("/application-test.yml")
 public class WardrobeControllerTest {
@@ -49,8 +44,8 @@ public class WardrobeControllerTest {
     private WardrobeService wardrobeService;
 
     @Test
-    @DisplayName("Возвращать страницу demi-season")
-    public void testGetAllBySummer() throws Exception {
+    @DisplayName("Возвращать страницу summer-season")
+    public void testGetAllBySummerSeason() throws Exception {
 
         Season season = Season.SUMMER;
 
@@ -73,7 +68,7 @@ public class WardrobeControllerTest {
 
     @Test
     @DisplayName("Возвращать страницу demi-season")
-    public void testGetAllBySeason() throws Exception {
+    public void testGetAllByDemiSeason() throws Exception {
 
         Season season = Season.DEMI;
 
@@ -101,6 +96,124 @@ public class WardrobeControllerTest {
                 .andExpect(model().attribute("wardrobe", clothes))
                 .andDo(print());
 
+    }
+
+    @Test
+    @DisplayName("Возвращать весь wardrobe")
+    public void testGetAllWardrobe() throws Exception {
+        Wardrobe stuff1 = new Wardrobe(1L, "Coat", Type.COAT, Style.CASUAL, Season.DEMI,
+                "/home/vckazakova/JavaPractice/clothes-weather/src/main/resources/photos/1.jpeg");
+        Wardrobe stuff2 = new Wardrobe(2L, "Boots", Type.SHOES, Style.CHIC, Season.DEMI,
+                "/home/vckazakova/JavaPractice/clothes-weather/src/main/resources/photos/2.jpg");
+        Wardrobe stuff3 = new Wardrobe(3L, "Dress", Type.DRESS, Style.CASUAL, Season.DEMI, "https://ae01.alicdn.com/kf/HTB1egF7bznuK1RkSmFPq6AuzFXa3/-.jpg");
+
+        List<Wardrobe> clothes = new ArrayList<>();
+        clothes.add(stuff1);
+        clothes.add(stuff2);
+        clothes.add(stuff3);
+
+        when(wardrobeService.findAll()).thenReturn(clothes);
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/wardrobe");
+
+        mockMvc.perform(request.accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("wardrobe"))
+                .andExpect(MockMvcResultMatchers.view().name("wardrobe"))
+                .andExpect(content().string(Matchers.containsString("All your clothes here")))
+                .andExpect(model().attribute("wardrobe", clothes))
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("Возвращать форму для создания вещи")
+    public void testGetCreateStuffForm() throws Exception {
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/wardrobe/create");
+
+        mockMvc.perform(request.accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("create"))
+                .andExpect(MockMvcResultMatchers.view().name("create"))
+                .andExpect(content().string(Matchers.containsString("Create stuff")))
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("Создавать вещь и возвращать страницу wardrobe")
+    public void testCreateStuffAndRedirectToWardrobe() throws Exception {
+
+        Wardrobe wardrobe = new Wardrobe();
+        wardrobe.setStuff("Pants");
+        wardrobe.setType(Type.BOTTOM);
+        wardrobe.setStyle(Style.CASUAL);
+        wardrobe.setSeason(Season.WINTER);
+        wardrobe.setUrl("http://vk.com/vika_ub/photo");
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/wardrobe/create");
+
+        mockMvc.perform(request.content(String.valueOf(wardrobe))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlTemplate("/wardrobe"));
+    }
+
+    @Test
+    @DisplayName("Удалять вещь и возвращать страницу wardrobe")
+    public void testDeleteStuffAndRedirectToWardrobe() throws Exception {
+
+        Long id = 1L;
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/wardrobe/delete/{id}", id);
+
+        mockMvc.perform(request.accept("application/json"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlTemplate("/wardrobe"));
+    }
+
+    @Test
+    @DisplayName("Возвращать форму для обновления вещи")
+    public void testGetUpdateStuffForm() throws Exception {
+
+        Long id = 3L;
+        Wardrobe stuff = new Wardrobe(3L, "Dress", Type.DRESS, Style.CASUAL, Season.DEMI, "https://ae01.alicdn.com/kf/HTB1egF7bznuK1RkSmFPq6AuzFXa3/-.jpg");
+
+        when(wardrobeService.findStuffById(id)).thenReturn(java.util.Optional.of(stuff));
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/wardrobe/update/{id}", id);
+
+        mockMvc.perform(request.accept("application/json"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/html;charset=UTF-8"))
+                .andExpect(view().name("update"))
+                .andExpect(MockMvcResultMatchers.view().name("update"))
+                .andExpect(content().string(Matchers.containsString("Please, update information about stuff!")))
+                .andDo(print());
+
+    }
+
+    @Test
+    @DisplayName("Обновлять вещь и возвращать страницу wardrobe")
+    public void testUpdateStuffAndRedirectToWardrobe() throws Exception {
+
+
+        Wardrobe wardrobe = new Wardrobe();
+        wardrobe.setStuff("Pants");
+        wardrobe.setType(Type.BOTTOM);
+        wardrobe.setStyle(Style.CASUAL);
+        wardrobe.setSeason(Season.WINTER);
+        wardrobe.setUrl("http://vk.com/vika_ub/photo");
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.post("/wardrobe/update");
+
+        mockMvc.perform(request.content(String.valueOf(wardrobe))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrlTemplate("/wardrobe"));
     }
 
 
