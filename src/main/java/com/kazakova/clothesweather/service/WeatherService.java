1dfb162674c83cd24a6a7aea3312d4bf224f73ef
VCKazakova <v.c.kazakova@gmail.com>
2021-12-08 12:49:36 +0300

    master add loggs

--- src/main/java/com/kazakova/clothesweather/service/WeatherService.java
+++ src/main/java/com/kazakova/clothesweather/service/WeatherService.java
@@ -21,24 +21,28 @@ import java.util.stream.Collectors;
 public class WeatherService {
 
     public static Document getPage() {
+        log.info(">> WeatherService getPage");
         try {
             String url = "https://pogoda.mail.ru/prognoz/moskva/";
             Document page = Jsoup.parse(new URL(url), 100000);
+            log.info("<< WeatherService getPage page={}", page);
             return page;
         } catch (Exception exception) {
-            log.error(">> WeatherService getPage exception={}", exception.getClass().getName());
+            log.error("<< WeatherService getPage exception={}", exception.getClass().getName());
         }
         return null;
     }
 
 
     public static Elements selectInformationAboutTodayWeather(Document page) {
+        log.info(">> WeatherService selectInformationAboutTodayWeather");
         return Objects.requireNonNull(page.select("div[class=information__content]")
                         .first())
                 .select("div[class=information__content__temperature]");
     }
 
     public String getTodayWeather() {
+        log.info(">> WeatherService getTodayWeather");
         try {
             Elements values = selectInformationAboutTodayWeather(Objects.requireNonNull(getPage()));
             String todayTemperature = values.text();
@@ -53,34 +57,42 @@ public class WeatherService {
             Matcher m = r.matcher(stringFromList);
 
             if (m.find()) {
+                log.info("<< WeatherService getTodayWeather todayTemperature={}", todayTemperature);
                 return "Погода на сегодня: " + todayTemperature + " " + m.group(0);
             } else throw new IOException("The weather doesn't find");
         } catch (IOException exception) {
-            log.error(">> WeatherService getTodayWeather exception={}", exception.getClass().getName());
+            log.error("<< WeatherService getTodayWeather exception={}", exception.getClass().getName());
         }
         return null;
     }
 
     public int getTodayTemperature() {
+        log.info(">> WeatherService getTodayTemperature");
         try {
             String todayWeather = getTodayWeather();
             String[] splitTodayWeather = todayWeather.split(" ");
             String stringOfTemperature = splitTodayWeather[3];
             String stringOfTemperatureWithoutLastElem = stringOfTemperature.substring(0, stringOfTemperature.length() - 1);
             int i = Integer.parseInt(stringOfTemperatureWithoutLastElem);
+            log.info(">> WeatherService getTodayTemperature temperature={}", i);
             return i;
         } catch (Exception exception) {
-            log.error(">> WeatherService getTodayWeather exception={}", exception.getClass().getName());
+            log.error("<< WeatherService getTodayWeather exception={}", exception.getClass().getName());
         }
         return 0;
     }
 
     public Season getWardrobeByWeather(int temperature) {
-        if (temperature >= -5 && temperature <= 17)
+        log.info(">> WeatherService getWardrobeByWeather temperature={}", temperature);
+        if (temperature >= -5 && temperature <= 17) {
+            log.info("<< WeatherService getWardrobeByWeather SEASON IS DEMISEASON");
             return Season.DEMI;
-        else if (temperature <= -6)
+        } else if (temperature <= -6) {
+            log.info("<< WeatherService getWardrobeByWeather SEASON IS WINTER");
             return Season.WINTER;
-        else return Season.SUMMER;
+        } else
+            log.info("<< WeatherService getWardrobeByWeather SEASON IS SUMMER");
+        return Season.SUMMER;
     }
 
 }
